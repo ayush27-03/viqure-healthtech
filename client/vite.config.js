ef80b3e4bd6c567e7aea11ba3bfce43d12839d20
DemoProjects <bakshivibhay@gmail.com>
2026-06-20 02:54:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // defaultPort: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        // Temporary 5000 for backend testing. 
-        // Change to 5500 otherwise browser will never know that backend is on localhost:5500
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
