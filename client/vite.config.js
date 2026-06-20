d43fc9094567bb7acaaa37463da72c331aa002b4
am03 <dihydrogenperoxide2703@gmail.com>
2026-06-20 20:24:46 +0530

    ops(local):updated local deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_APP_API_URL,
+          target:'https://localhost:5500',
           changeOrigin: true,
           secure: false,
         }
